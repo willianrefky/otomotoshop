17ee6e874b947c9fbab6dcef035e2a413d176a65
NikwaF <nikonabilah@gmail.com>
2018-10-25 05:49:24 +0700

    acuma nambah dikit

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+  <h1>Otomotoshop</h1>
+  <!-- .nama -->
+  <div class="nama">
+    <h2>kami dari kelompok 6</h2>
+  </div>
+  <!-- /.nama -->
+
+</body>
+</html>
