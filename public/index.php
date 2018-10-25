2cc12d0553d6a16e9a98b6155ca9c349d30c5917
NikwaF <nikonabilah@gmail.com>
2018-10-25 13:36:40 +0700

    hapus nih

--- public/index.php
+++ public/index.php
@@ -5,11 +5,5 @@
   <title></title>
 </head>
 <body>
-  <h1>Otomotoshop</h1>
-  <div class="nama">
-    <h2>kami dari kelompok 6</h2>
-    <p>mencoba menjalankan repository github</p>
-  </div>
-
 </body>
 </html>
