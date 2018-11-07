885985292f64bc1b19898c459d4d6457dbd7356f
NikwaF <nikonabilah@gmail.com>
2018-11-07 22:17:23 +0700

    aconfig file

---
+++ app/configs/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('URLBASE', 'http://localhost/otomotoshop/index');
+
+//untuk database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'niko21');
+define('DB_NAME', 'otomotoshop');
